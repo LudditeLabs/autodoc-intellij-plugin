43deb9640e49ba7e9428557fdd9498463e44911c
Sergey Kozlov <skozlovf@gmail.com>
2017-06-06 22:47:52 +0600

    Involve platform bundles

--- src/com/ludditelabs/intellij/autodoc/PluginApp.java
+++ src/com/ludditelabs/intellij/autodoc/PluginApp.java
@@ -1,7 +1,9 @@
 package com.ludditelabs.intellij.autodoc;
 
+import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.components.ApplicationComponent;
 import com.intellij.openapi.diagnostic.Logger;
+import com.ludditelabs.intellij.autodoc.bundle.PluginBundleManager;
 import org.jetbrains.annotations.NotNull;
 
 
@@ -11,9 +13,30 @@ import org.jetbrains.annotations.NotNull;
 public class PluginApp implements ApplicationComponent {
     protected static final Logger logger = Logger.getInstance("ludditelabs.autodoc.PluginApp");
 
+    // Check for platform bundle and propose to update/download.
+    private void checkPlatformBundle() {
+        PluginBundleManager manager =
+            (PluginBundleManager)PluginBundleManager.getInstance();
+
+        if (!manager.getLocalBundle().isExist()) {
+            manager.showFirstDownloadNotification();
+        }
+        else {
+            // TODO: delay new version check.
+            // For example, if last check was less than 2 hrs then don't check.
+            manager.checkUpdateSilent();
+        }
+    }
+
+    /** Get plugin's application service instance. */
+    public static PluginApp getInstance() {
+        return ApplicationManager.getApplication().getComponent(
+            PluginApp.class);
+    }
+
     @Override
     public void initComponent() {
-
+        checkPlatformBundle();
     }
 
     @Override
